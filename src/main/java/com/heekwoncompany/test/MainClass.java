ea5d7749a1cdfb583bf5dc0dbb941e4d06f7d4c1
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-10-26 11:50:45 +0900

    DI 설정 연습 java 파일안에 xml 컨테이너 불러오기

--- src/main/java/com/heekwoncompany/test/MainClass.java
+++ src/main/java/com/heekwoncompany/test/MainClass.java
@@ -10,8 +10,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+//		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
 		
 		Student stu1 = ctx.getBean("student1",Student.class);
 		System.out.println(stu1.getName());
@@ -27,19 +27,19 @@ public class MainClass {
 		System.out.println(stu2.getHeight());
 		System.out.println(stu2.getWeight());
 		
-		Student stu3 = ctx.getBean("student3",Student.class);
-		System.out.println(stu3.getName());
-		System.out.println(stu3.getAge());
-		System.out.println(stu3.getHobbys());
-		System.out.println(stu3.getHeight());
-		System.out.println(stu3.getWeight());
+//		Student stu3 = ctx.getBean("student3",Student.class);
+//		System.out.println(stu3.getName());
+//		System.out.println(stu3.getAge());
+//		System.out.println(stu3.getHobbys());
+//		System.out.println(stu3.getHeight());
+//		System.out.println(stu3.getWeight());
 		
-//		Student stu4 = ctx.getBean("student4",Student.class);
-//		System.out.println(stu4.getName());
-//		System.out.println(stu4.getAge());
-//		System.out.println(stu4.getHobbys());
-//		System.out.println(stu4.getHeight());
-//		System.out.println(stu4.getWeight());
+		Student stu4 = ctx.getBean("student4",Student.class);
+		System.out.println(stu4.getName());
+		System.out.println(stu4.getAge());
+		System.out.println(stu4.getHobbys());
+		System.out.println(stu4.getHeight());
+		System.out.println(stu4.getWeight());
 	}
 
 }
